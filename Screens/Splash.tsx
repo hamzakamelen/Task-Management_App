1e9758e083204cab9100d8d4acb0146ee0949b97
Hamza Kamelen <hamzakamelen12345@gmail.com>
2023-09-07 22:55:28 +0500

    Done

--- Screens/Splash.tsx
+++ Screens/Splash.tsx
@@ -10,13 +10,12 @@ function Splash({ navigation }: any) {
                     <Text style={[styles.textBold, { fontSize: 40 }, styles.textPrimary, styles.my1, styles.textCenter]}>Taskcy</Text>
                     <Text style={[styles.fs1, styles.textBlack, styles.textBold, styles.textCenter]}>Building Better</Text>
                     <Text style={[styles.fs1, styles.textBlack, styles.textBold, styles.textCenter]}>Workplaces</Text>
-                    <Text style={[styles.flexCenter, styles.my1, styles.justifyContentCenter,styles.fs, { color: "lightgrey", marginLeft: 32 }]}>Create a unique emotional story that describe better than words</Text>
+                    <Text style={[styles.flexCenter, styles.my1, styles.justifyContentCenter,styles.fs,styles.textBold, { color: "darkgrey", marginLeft: 32 }]}>Create a unique emotional story that describe better than words</Text>
                     <TouchableOpacity onPress={() => navigation.navigate('Intro1')} style={[styles.shadowEx, styles.bgPrimary, styles.w100, styles.my2, styles.justifyContentCenter, { borderRadius: 20, shadowOpacity: 1, shadowColor: '#6f6aee', height: 50 }]}>
                         <Text style={[styles.textCenter, styles.textBold, styles.fs4, styles.textWhite]}>Get Started</Text>
                     </TouchableOpacity>
                 </View>
             </View>
-
         </>
     )
 }
--- navigation/navigation.tsx
+++ navigation/navigation.tsx
@@ -15,36 +15,37 @@ const Navigation = () => {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Splash">
                 <Stack.Screen options={{
-                    headerShown: true,
-                    headerTitleAlign: "center",
-                    headerShadowVisible:false,
-                    headerTitle: "Sign In",
-                }} name="Login" component={Login} />
-
-                <Stack.Screen  options={{
-                    headerShown: true,
-                    headerTitleAlign: "center",
-                    headerShadowVisible:false
-                }} name="SignUp" component={Signup} />
-                
-                <Stack.Screen options={{
                     headerShown: false
                 }} name="Splash" component={Splash} />
-                
+
                 <Stack.Screen options={{
                     headerShown: false
                 }} name="Intro1" component={Intro1} />
-                
+
                 <Stack.Screen options={{
                     headerShown: false
                 }} name="Intro2" component={Intro2} />
-                
+
                 <Stack.Screen options={{
                     headerShown: false
                 }} name="Intro3" component={Intro3} />
-               
+
                 <Stack.Screen options={{
-                   headerShown:false
+                    headerShown: true,
+                    headerBackVisible:false,
+                    headerTitleAlign: "center",
+                    headerShadowVisible: false,
+                    headerTitle: "Sign In",
+                }} name="Login" component={Login} />
+
+                <Stack.Screen options={{
+                    headerShown: true,
+                    headerBackVisible:false,
+                    headerTitleAlign: "center",
+                    headerShadowVisible: false
+                }} name="SignUp" component={Signup} />
+                <Stack.Screen options={{
+                    headerShown: false
                 }} name="Home" component={TabNavigation} />
 
             </Stack.Navigator>
@@ -53,61 +54,3 @@ const Navigation = () => {
 }
 
 export default Navigation
-
-{/* <Tab.Screen name="Home" component={Home}
-options={{
-    tabBarIcon: () => (
-        <View>
-            <View style={[{ marginLeft: 3 }]}>
-                <HMIcon name='home' size={30} color="grey" />
-            </View>
-            <Text>Home</Text>
-        </View>
-    )
-}}
-/>
-<Tab.Screen name="Search" component={Search}
-options={{
-    tabBarIcon: () => (
-        <View>
-            <View style={{ marginLeft: 5 }}>
-                <HMIcon name='search' size={30} color="grey" />
-            </View>
-            <Text>Search</Text>
-        </View>
-    )
-}}
-/>
-<Tab.Screen name="Post" component={Post}
-options={{
-    tabBarIcon: () => (
-        <View style={[{ marginTop: -8 }]}>
-            <View style={[styles.bgDanger, styles.roundedPill, { marginLeft: -4 }]}>
-                <HMIcon name='add' size={45} color="white" />
-            </View>
-        </View>
-    )
-}}
-/>
-<Tab.Screen name="Notification" component={Notification}
-options={{
-    tabBarIcon: () => (
-        <View>
-            <View style={{ marginLeft: 8 }}>
-                <HMIcon name='favorite-outline' size={30} color="grey" />
-            </View>
-            <Text>Activity</Text>
-        </View>
-    )
-}} />
-<Tab.Screen name="Profile" component={Profile}
-options={{
-    tabBarIcon: () => (
-        <View>
-            <View style={{ marginLeft: 5 }}>
-                <HMIcon name='person-outline' size={30} color="grey" />
-            </View>
-            <Text>Profile</Text>
-        </View>
-    )
-}} /> */}
